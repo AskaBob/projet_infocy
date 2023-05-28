54d6e75eaef55837c1fa2ed57690ac3201194074
AskaBob <105872993+AskaBob@users.noreply.github.com>
2023-05-28 23:24:45 +0200

    main.c

--- main.c
+++ main.c
@@ -7,17 +7,31 @@
 #include "save.h"
 
 int main() {
-    srand(time(NULL));
-    TitleScreen();
-    ChooseCharacter();
-
-    char nameFI[] = "chara/swordf.txt";
-    char nameFIM[] = "chara/slime.txt";
     Inventory inventory;
     Monster monster;
+    srand(time(NULL));
+    TitleScreen();
+    if(ChooseCharacter() == 2){
+        char nameFI[] = "chara/swordw.txt";
+    }
+    else{
+        char nameFI[] = "chara/staffw.txt";
+    }
 
     InitCharacter(nameFI, &inventory);
     InitMonster(nameFIM, &monster);
+    
+    ReadText("description/welcome.txt");
+    ReadText("choice/choice1.txt");
+    if(MakeChoice() == 0){
+        ReadText("description/peacefullife.txt");
+        printf("FIN 'Vie paisible'");
+    }
+    else if(MakeChoice() == 1){
+        ReadText("description/begintrip.txt");
+        Fight(&monster, &inventory);
+    }
+    
 
     printf("Nom du monstre : %s\n", monster.name);
     printf("Points de vie du monstre : %d\n", monster.life);
